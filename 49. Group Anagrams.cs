ba27944f8c662ae2b31c5e913ae97d722d9a8f10
Sangjae Lee <robin040416@gmail.com>
2024-11-04 19:21:18 -0500

    Create 49. Group Anagrams.cs
    Leetcode Group Anagrams.cs

---
+++ 49. Group Anagrams.cs
@@ -0,0 +1,27 @@
+﻿public class Solution 
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs) 
+    {
+        var rest = new Dictionary<string, List<string>>();
+
+        foreach (string s in strs)
+        {
+            int[] arr = new int[26];
+            foreach (char c in s)
+            {
+                arr[c-'a']++;
+            }
+            string returnVal = new string(String.Join(",", arr));
+            if (!rest.ContainsKey(returnVal))
+            {
+                rest[returnVal] = new List<string>();
+            }
+            else
+            {
+                rest[returnVal].Add(s);
+            }
+        }
+
+        return rest.Values.ToList<IList<String>>();   
+    }
+}
