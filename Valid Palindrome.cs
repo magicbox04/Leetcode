0d00a7d266f4f900970c59a9bed13d77c524f167
Sangjae Lee <robin040416@gmail.com>
2024-11-20 19:01:57 -0500

    Create Valid Palindrome.cs

---
+++ Valid Palindrome.cs
@@ -0,0 +1,28 @@
+public class Solution 
+{
+    public bool IsPalindrome(string s) 
+    {
+        int j = s.Length - 1;
+        int i = 0;
+        while (i < j)
+        {
+            while (i < j && !char.IsLetterOrDigit(s[i]))
+            {
+                i++;
+            }
+            char c = s[i];
+            while (i < j && !char.IsLetterOrDigit(s[j]))
+            {
+                j--;
+            }
+            char d = s[j];
+            if (char.ToLower(s[i]) != char.ToLower(s[j]))
+            {
+                return false;
+            }
+            j--;
+            i++;
+        }
+        return true;
+    }
+}
